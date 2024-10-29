a19ca446dac53041143a259c1f3533447f37c0d5
CAVVEX0 <120098053+CAVVEX0@users.noreply.github.com>
2024-10-29 16:23:05 +0100

    sds

--- hooks/useColor.ts
+++ hooks/useColor.ts
@@ -1,18 +1,11 @@
+// useColor.ts
 export const getColorClassByUser = (name: string) => {
-  switch (name.toLowerCase()) {
-    case "adam":
-      return "bg-red-500 text-white";
-    case "yassine":
-      return "bg-blue-500 text-white";
-    case "hannan":
-      return "bg-green-500 text-white";
-    case "amine":
-      return "bg-yellow-500";
-    case "fija":
-      return "bg-purple-500 text-white";
-    case "david":
-      return "bg-pink-500";
-    default:
-      return "bg-gray-300"; // Fallback color
-  }
+  // Explicitly list all possible classes to prevent purging
+  if (name.toLowerCase() === "adam") return "bg-red-500 text-white";
+  if (name.toLowerCase() === "yassine") return "bg-blue-500 text-white";
+  if (name.toLowerCase() === "hannan") return "bg-green-500 text-white";
+  if (name.toLowerCase() === "amine") return "bg-yellow-500";
+  if (name.toLowerCase() === "fija") return "bg-purple-500 text-white";
+  if (name.toLowerCase() === "david") return "bg-pink-500 text-white";
+  return "bg-gray-300 text-gray-500"; // Default color
 };
