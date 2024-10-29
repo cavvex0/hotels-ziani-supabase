a3eccdc78c4b2eb46bd0663f3551de35dce07710
CAVVEX0 <120098053+CAVVEX0@users.noreply.github.com>
2024-10-29 16:35:24 +0100

    sd

--- hooks/useColor.ts
+++ hooks/useColor.ts
@@ -3,8 +3,8 @@ export const getColorByUser = (name: string): string => {
     const colorMap: Record<string, string> = {
       adam: "#ef4444", // red-500
       yassine: "#3b82f6", // blue-500
-      hannan: "#10b981", // green-500
-      amine: "#fbbf24", // yellow-500
+      amine: "#10b981", // green-500
+      hannan: "#fbbf24", // yellow-500
       fija: "#a855f7", // purple-500
       david: "#ec4899", // pink-500
       default: "#d1d5db", // gray-300
